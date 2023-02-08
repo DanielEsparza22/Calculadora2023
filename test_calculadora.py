d8e4569030776161ca9e1c273167635b9f7a6b10
Daniel Esparza Medina <38192398@uaz.edu.mx>
2023-02-08 14:01:32 -0600

    Prueba de 10 + 2

--- test_calculadora.py
+++ test_calculadora.py
@@ -6,5 +6,13 @@ class TestCalculadora(unittest.TestCase):
         calc = Calculadora()
         self.assertEqual(4,calc.sumar(2,2))
 
+class TestCalculadora(unittest.TestCase):
+    def test_2_mas_2(self):
+        calc = Calculadora()
+        self.assertEqual(11,calc.sumar(10,2))
+
 if __name__ == '__main__':
     unittest.main()
+
+
+#52.40.132.229
